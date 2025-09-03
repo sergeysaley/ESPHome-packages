6eb4a61505cf4c08d7d227377a27dbd892082520
Serhii Saliei <70771776+sergeysaley@users.noreply.github.com>
2025-09-03 17:33:16 +0300

    Update vl53l1x_sensor.h

--- components/vl53l1x_sensor/vl53l1x_sensor.h
+++ components/vl53l1x_sensor/vl53l1x_sensor.h
@@ -1,6 +1,6 @@
 #include "esphome/core/component.h"
-#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
+#include "esphome/components/sensor/sensor.h"
 #include <VL53L1X.h>
 
 namespace esphome {
@@ -8,12 +8,13 @@ namespace vl53l1x_sensor {
 
 class VL53L1XSensor : public PollingComponent, public i2c::I2CDevice {
  public:
-  Sensor *distance_sensor = new Sensor();
-
   VL53L1XSensor() : PollingComponent(1000) {}
+
   void setup() override;
   void update() override;
-};
+
+  // Pointer на sensor об’єкт, який реєструється у Python
+  sensor::Sensor *distance_sensor = nullptr;
 
  protected:
   VL53L1X vl_;
