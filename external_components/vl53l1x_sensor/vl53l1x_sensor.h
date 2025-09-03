d3ae8804d6a8d38971fbaf16c213aa256eac1934
Serhii Saliei <70771776+sergeysaley@users.noreply.github.com>
2025-09-03 17:10:03 +0300

    Create vl53l1x_sensor.h

---
+++ external_components/vl53l1x_sensor/vl53l1x_sensor.h
@@ -0,0 +1,21 @@
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"
+#include <VL53L1X.h>
+
+namespace esphome {
+namespace vl53l1x_sensor {
+
+class VL53L1XSensor : public PollingComponent, public i2c::I2CDevice, public sensor::Sensor {
+ public:
+  VL53L1XSensor() : PollingComponent(1000) {}
+
+  void setup() override;
+  void update() override;
+
+ protected:
+  VL53L1X vl_;
+};
+
+}  // namespace vl53l1x_sensor
+}  // namespace esphome
